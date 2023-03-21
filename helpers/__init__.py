19854c83578c258e1be87cd42f5775535fb61ce7
LeoMaurice <leopold-maurice@outlook.fr>
2023-03-21 19:53:48 +0100

    maj init

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -5,15 +5,12 @@ os.environ['USE_PYGEOS'] = '0'
 
 from . import scrapping
 from . import visualize
-from . import scrapping_big
 
 #to assure that reload(helpers) reloads everything in the folder.
 from importlib import reload
 reload(scrapping)
 reload(visualize)
-reload(scrapping_big)
 
 #to not have to import each file separetly.
 from .scrapping import *
 from .visualize import *
-from .scrapping_big import *
